d9603e765323820fa94ad611304606f22f0fb4dc
jskang <kjuns1@naver.com>
2021-10-14 22:57:04 +0900

    feat(file): file upload,download function create.

--- src/main/java/com/jskang/storagenode/api.java
+++ src/main/java/com/jskang/storagenode/api.java
@@ -1,9 +1,13 @@
 package com.jskang.storagenode;
 
+import com.jskang.storagenode.file.Download;
+import com.jskang.storagenode.file.Upload;
 import com.jskang.storagenode.node.Node;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.server.RequestPredicates;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.RouterFunctions;
 import org.springframework.web.reactive.function.server.ServerResponse;
@@ -22,6 +26,8 @@ public class api {
             .POST("node/join", request -> node.networkJoin(
                 request.queryParam("ip").get(), Integer.valueOf(request.queryParam("port").get()))
             )
+            .POST("file/upload", RequestPredicates.accept(MediaType.MULTIPART_FORM_DATA), request -> new Upload().fileUpload(request))
+            .POST("file/download", RequestPredicates.accept(MediaType.TEXT_PLAIN), request -> new Download().fileDownload())
             .build();
     }
 }
---
+++ src/main/java/com/jskang/storagenode/file/Download.java
@@ -0,0 +1,20 @@
+package com.jskang.storagenode.file;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Mono;
+
+public class Download {
+    private Logger LOG = LoggerFactory.getLogger(this.getClass());
+
+    /**
+     * File download
+     * @return if upload ok 'success', others 'fail'
+     */
+    public Mono<ServerResponse> fileDownload() {
+        LOG.info("File download start.");
+        return null;
+    }
+
+}
