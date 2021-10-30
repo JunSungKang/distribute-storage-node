d99d06b0c30014f79b2bc80b13d1813c79c72963
jskang <kjuns1@naver.com>
2021-10-30 14:35:07 +0900

    feat(file): file upload add. max data size 10Gb.

--- src/main/java/com/jskang/storagenode/file/Upload.java
+++ src/main/java/com/jskang/storagenode/file/Upload.java
@@ -1,7 +1,15 @@
 package com.jskang.storagenode.file;
 
+import static org.springframework.web.reactive.function.server.ServerResponse.ok;
+
+import java.nio.file.Paths;
+import java.util.Map;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.codec.multipart.FilePart;
+import org.springframework.http.codec.multipart.Part;
+import org.springframework.web.reactive.function.BodyExtractors;
+import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
 import reactor.core.publisher.Mono;
@@ -17,7 +25,16 @@ public class Upload {
      * @return if upload ok 'success', others 'fail'
      */
     public Mono<ServerResponse> fileUpload(ServerRequest request) {
-        LOG.info("File upload start.");
-        return null;
+        return request.body(BodyExtractors.toMultipartData())
+            .flatMap(parts -> {
+                Map<String, Part> map = parts.toSingleValueMap();
+                if (map.get("file") instanceof FilePart) {
+                    FilePart filePart = (FilePart) map.get("file");
+                    return filePart.transferTo( Paths.get("upload\\" +filePart.filename()) );
+                }
+                return null;
+            })
+            .then(ok().body(
+                BodyInserters.fromProducer(Mono.just("{\"statusCode\": 200}"), String.class)));
     }
 }
