65542e33981cee32d61210f06b7c62bcdd6054d5
jskang <kjuns1@naver.com>
2021-11-01 23:02:47 +0900

    feat(file): file download add.

--- src/main/java/com/jskang/storagenode/api.java
+++ src/main/java/com/jskang/storagenode/api.java
@@ -1,16 +1,23 @@
 package com.jskang.storagenode;
 
 import com.jskang.storagenode.file.Download;
+import com.jskang.storagenode.file.FileManage;
 import com.jskang.storagenode.file.Upload;
 import com.jskang.storagenode.node.Node;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.server.RequestPredicates;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.RouterFunctions;
 import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Mono;
 
 @Configuration
 @Component
@@ -27,9 +34,30 @@ public class api {
             .POST("file/upload",
                 RequestPredicates.accept(MediaType.MULTIPART_FORM_DATA),
                 request -> new Upload().fileUpload(request))
-            .POST("file/download",
-                RequestPredicates.accept(MediaType.TEXT_PLAIN),
-                request -> new Download().fileDownload())
+            .GET("file/position_info",
+                RequestPredicates.accept(MediaType.APPLICATION_JSON),
+                request -> {
+                    Optional<String> optionalFileName = request.queryParam("fileName");
+                    if (optionalFileName.isPresent()) {
+                        String fileName = optionalFileName.get();
+                        List<String> positions = (List<String>) FileManage
+                            .getFilePostionStream(fileName)
+                            .map(o -> ((Path) o).getFileName().toString())
+                            .collect(Collectors.toList());
+
+                        return ServerResponse.ok()
+                            .body(BodyInserters
+                                .fromProducer(Mono.just(positions), List.class));
+                    } else {
+                        return ServerResponse.badRequest().body(
+                            BodyInserters
+                                .fromProducer(Mono.just("{\"statusCode\": 400}"), String.class)
+                        );
+                    }
+                })
+            .GET("file/download",
+                RequestPredicates.accept(MediaType.APPLICATION_JSON),
+                request -> new Download().fileDownload(request))
             .build();
     }
 }
--- src/main/java/com/jskang/storagenode/file/Download.java
+++ src/main/java/com/jskang/storagenode/file/Download.java
@@ -1,7 +1,13 @@
 package com.jskang.storagenode.file;
 
+import java.util.Optional;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.web.reactive.function.BodyInserters;
+import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
 import reactor.core.publisher.Mono;
 
@@ -14,9 +20,27 @@ public class Download {
      *
      * @return if upload ok 'success', others 'fail'
      */
-    public Mono<ServerResponse> fileDownload() {
-        LOG.info("File download start.");
-        return null;
+    public Mono<ServerResponse> fileDownload(ServerRequest request) {
+        Optional<String> optionalFileName = request.queryParam("fileName");
+
+        if (optionalFileName.isPresent()) {
+            String fileName = optionalFileName.get();
+            LOG.info("File '" + fileName + "' download start.");
+
+            Resource resource = new FileSystemResource("upload\\" + fileName);
+            Mono<Resource> mapper = Mono.just(resource);
+
+            Mono<ServerResponse> res = ServerResponse.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION,
+                    "attachment; filename=\"" + fileName + "\"")
+                .body(BodyInserters.fromProducer(mapper, Resource.class));
+            return res;
+        } else {
+            LOG.error("request query 'fileName' empty.");
+            return ServerResponse.badRequest().body(
+                BodyInserters.fromProducer(Mono.just("{\"statusCode\": 400}"), String.class)
+            );
+        }
     }
 
 }
