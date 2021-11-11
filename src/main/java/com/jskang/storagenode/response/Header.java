969f98d8c8864b55fe67a4826d7a05bde9d9a119
jskang <kjuns1@naver.com>
2021-11-12 03:08:18 +0900

    feat(response): request api result class add.

---
+++ src/main/java/com/jskang/storagenode/response/Header.java
@@ -0,0 +1,20 @@
+package com.jskang.storagenode.response;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonRootName;
+
+@JsonRootName("header")
+public class Header {
+
+    @JsonProperty("code")
+    private int code;
+    @JsonProperty("message")
+    private String message;
+
+    public Header(
+        @JsonProperty("code") int code,
+        @JsonProperty("message") String message) {
+        this.code = code;
+        this.message = message;
+    }
+}
---
+++ src/main/java/com/jskang/storagenode/response/ResponseResult.java
@@ -0,0 +1,48 @@
+package com.jskang.storagenode.response;
+
+import static org.springframework.web.reactive.function.server.ServerResponse.ok;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonRootName;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.reactive.function.BodyInserters;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Mono;
+
+public class ResponseResult {
+
+    public static Mono<ServerResponse> success(Object data) {
+        Header header = new Header(HttpStatus.OK.value(), HttpStatus.OK.getReasonPhrase());
+        return ok().bodyValue(new ResponseData(header, data));
+    }
+
+    public static Mono<ServerResponse> fail(HttpStatus httpStatus) {
+        Header header = new Header(httpStatus.value(), httpStatus.getReasonPhrase());
+        return ok().bodyValue(new ResponseData(header, ""));
+    }
+
+    public static Mono<ServerResponse> download(Mono<Resource> resourceMono, String fileName) {
+        return ok().header(HttpHeaders.CONTENT_DISPOSITION,
+            "attachment; filename=\"" + fileName + "\"")
+            .body(BodyInserters.fromProducer(resourceMono, Resource.class));
+    }
+
+    @JsonRootName("response")
+    public static class ResponseData {
+
+        @JsonProperty("header")
+        private Header header;
+        @JsonProperty("body")
+        private Object body;
+
+        private ResponseData(
+            @JsonProperty("header") Header header,
+            @JsonProperty("body") Object body) {
+            this.header = header;
+            this.body = body;
+        }
+    }
+
+}
